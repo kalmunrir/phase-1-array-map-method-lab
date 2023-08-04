a0b6b81026b4515aead1a28d6448bb7f30117dad
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-04 11:57:28 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => wordStartUpper(title));
+}
+
+function wordStartUpper(title) {
+  let words = title.split(` `);
+
+  words = words.map(word => `${word.charAt(0).toUpperCase()}${word.slice(1)}`);
+
+  return words.join(` `);
 }
